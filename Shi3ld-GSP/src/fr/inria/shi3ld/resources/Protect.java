18a2df3cf0823d5cb9769c7da07977d215be59c7
Luca Costabello <lukostaz@gmail.com>
2013-06-03 11:32:40 +0200

    Added GPL licence to src

--- Shi3ld-GSP/src/fr/inria/shi3ld/resources/Protect.java
+++ Shi3ld-GSP/src/fr/inria/shi3ld/resources/Protect.java
@@ -1,3 +1,27 @@
+/**
+ * Shi3ld - Shi3ld for HTTP is an access control module for enforcing 
+ * authorization on triple stores. Shi3ld for HTTP protects HTTP operations 
+ * on Linked Data and relies on attribute-based access policies.
+ *
+ * Copyright (C) 2013 Luca Costabello, Serena Villata,
+ *  Oscar Rodriguez-Rocha, Fabien Gandon - v1.0
+ * *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
 package fr.inria.shi3ld.resources;
 
 import com.sun.jersey.core.util.Base64;
